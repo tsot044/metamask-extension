6acfbfa287fc5d205d337add3b55094e1df0ced1
Howard Braham <howrad@gmail.com>
2025-05-16 16:16:38 -0400

    not right

--- ui/components/app/alert-system/contexts/alertActionHandler.test.ts
+++ ui/components/app/alert-system/contexts/alertActionHandler.test.ts
@@ -2,11 +2,6 @@ import React from 'react';
 import { renderHookWithProvider } from '../../../../../test/lib/render-helpers';
 import { useAlertActionHandler } from './alertActionHandler';
 
-jest.mock('react', () => ({
-  ...jest.requireActual('react'),
-  useContext: jest.fn(),
-}));
-
 describe('alertActionHandler', () => {
   it('throws an error if used outside of AlertActionHandlerProvider', () => {
     const { result } = renderHookWithProvider(() => useAlertActionHandler());
@@ -18,6 +13,11 @@ describe('alertActionHandler', () => {
   });
 
   it('returns the context value when used within AlertActionHandlerProvider', () => {
+    jest.mock('react', () => ({
+      ...jest.requireActual('react'),
+      useContext: jest.fn(),
+    }));
+
     const mockProcessAction = jest.fn();
     (React.useContext as jest.Mock).mockReturnValue({
       processAction: mockProcessAction,
