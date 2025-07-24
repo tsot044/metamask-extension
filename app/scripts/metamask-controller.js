3cc12de58b65a9003b7490b47d2e9860375e4018
Elliot Winkler <elliot.winkler@gmail.com>
2025-07-24 14:43:53 -0600

    WIP - Extend metrics for RPC endpoints

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -746,11 +746,9 @@ export default class MetamaskController extends EventEmitter {
     });
     networkControllerMessenger.subscribe(
       'NetworkController:rpcEndpointUnavailable',
-      async ({ chainId, endpointUrl, error }) => {
+      async (data) => {
         onRpcEndpointUnavailable({
-          chainId,
-          endpointUrl,
-          error,
+          ...data,
           infuraProjectId: opts.infuraProjectId,
           trackEvent: this.metaMetricsController.trackEvent.bind(
             this.metaMetricsController,
@@ -761,10 +759,9 @@ export default class MetamaskController extends EventEmitter {
     );
     networkControllerMessenger.subscribe(
       'NetworkController:rpcEndpointDegraded',
-      async ({ chainId, endpointUrl }) => {
+      async (data) => {
         onRpcEndpointDegraded({
-          chainId,
-          endpointUrl,
+          ...data,
           infuraProjectId: opts.infuraProjectId,
           trackEvent: this.metaMetricsController.trackEvent.bind(
             this.metaMetricsController,
